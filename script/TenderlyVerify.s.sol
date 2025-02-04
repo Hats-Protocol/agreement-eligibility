951fe6c61c86054ff67fc1ea6dcf31e3520c5a18
spengrah <code@spengrah.xyz>
2025-02-04 15:05:07 -0600

    fix ci

--- script/TenderlyVerify.s.sol
+++ script/TenderlyVerify.s.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { Script } from "forge-std/Script.sol";
-import { console2 } from "forge-std/Console2.sol";
+import { Script, console2 } from "forge-std/Script.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 
 contract TenderlyVerifyScript is Script {
